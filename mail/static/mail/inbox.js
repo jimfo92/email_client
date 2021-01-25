7618093f636b394d3bee80eb2704d10de5dca8fd
Dimitris Tsifo <dimitristsifo@MacBook-Pro.station>
2021-01-26 00:48:18 +0200

    compose completed

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  // When form submited, take values and send them to /emails
+  document.querySelector('#compose-form').onsubmit = () => {
+    let recipient_list = document.querySelector('#compose-recipients').value;
+    let sb = document.querySelector('#compose-subject').value;
+    let bd = document.querySelector('#compose-body').value;
+
+    // data to be sent to the POST request
+    let _data = {
+      recipients: recipient_list,
+      subject: sb,
+      body: bd
+    }
+
+    fetch('/emails', {
+      method: "POST",
+      body: JSON.stringify(_data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json));
+
+  load_mailbox('inbox');
+
+  // not submit the form
+  return false;
+  }
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
