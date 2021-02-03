eacb92ea051f3c6722f5a97b0592c67d970c58ee
jimfo92 <dimtsif92@gmail.com>
2021-02-04 00:37:00 +0200

    fetch mailbax completed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,34 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // When form submited, take values and send them to /emails
+  document.querySelector('#compose-form').onsubmit = () => {
+    let recipient_list = document.querySelector('#compose-recipients').value;
+    let sb = document.querySelector('#compose-subject').value;
+    let bd = document.querySelector('#compose-body').value;
+
+    // data to be sent to the POST request
+    let _data = {
+      recipients: recipient_list,
+      subject: sb,
+      body: bd,
+      read: false
+    }
+
+    fetch('/emails', {
+      method: "POST",
+      body: JSON.stringify(_data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json));
+
+    load_mailbox('sent');
+
+    // not submit the form
+    return false;
+  }
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -20,32 +48,7 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 
-  // When form submited, take values and send them to /emails
-  document.querySelector('#compose-form').onsubmit = () => {
-    let recipient_list = document.querySelector('#compose-recipients').value;
-    let sb = document.querySelector('#compose-subject').value;
-    let bd = document.querySelector('#compose-body').value;
-
-    // data to be sent to the POST request
-    let _data = {
-      recipients: recipient_list,
-      subject: sb,
-      body: bd
-    }
-
-    fetch('/emails', {
-      method: "POST",
-      body: JSON.stringify(_data),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-    .then(response => response.json()) 
-    .then(json => console.log(json));
-
-  load_mailbox('inbox');
-
-  // not submit the form
-  return false;
-  }
+  
 }
 
 function load_mailbox(mailbox) {
@@ -56,4 +59,22 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // load mailbox
+  fetch(`emails/${mailbox}`).then(response => response.json()).then(emails => {
+    emails.forEach(email => {
+      let div = document.createElement('div');
+      div.className = 'mail_div';
+      div.innerHTML =  `<div><span id="sender">${email.sender}</span> <span id="subject">${email.subject}</span>
+      <span id="timestamp">${email.timestamp}</span></div>`;
+      //if the email is unread make background gray
+      if (email.read === true) {
+        div.style.background = "#d3d3d3" ;
+      }
+      document.querySelector('#emails-view').appendChild(div);
+      div.addEventListener('click', function() {
+        console.log("You clicked me");
+      })
+    });
+  })
 }
