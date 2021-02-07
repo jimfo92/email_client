b4d3a48c09c2e2e2744aa48b86640a20f5f75e07
jimfo92 <dimtsif92@gmail.com>
2021-02-08 01:03:52 +0200

    archive incomplete

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -48,12 +48,10 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-
   
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -64,28 +62,37 @@ function load_mailbox(mailbox) {
   
   // load mailbox
   fetch(`emails/${mailbox}`).then(response => response.json()).then(emails => {
+    console.log(emails);
     emails.forEach(email => {
       let div = document.createElement('div');
       div.className = 'mail_div';
       div.innerHTML =  `<div><span id="sender">${email.sender}</span> <span id="subject">${email.subject}</span>
       <span id="timestamp">${email.timestamp}</span></div>`;
+
       //if the email is unread make background gray
       if (email.read === true) {
         div.style.background = "#d3d3d3" ;
       }
       document.querySelector('#emails-view').appendChild(div);
       div.addEventListener('click', function() {
-        load_email(email.id);
+        load_email(email.id, mailbox);
       })
     });
   })
 }
 
-function load_email(email_id) {
+function load_email(email_id, mailbox) {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#em_view').style.display = 'block';
 
+  //set archived button if we are to inbox mailbox
+  document.querySelector('#archived_button').style.display = 'none';
+  if (mailbox === 'inbox') {
+    document.querySelector('#archived_button').style.display = 'block';
+  }
+
+  //fetch email details
   fetch(`emails/${email_id}`).then(response => response.json()).then(data => {
     console.log(data);
     document.querySelector('#email_subject').innerHTML = data.subject;
@@ -94,4 +101,25 @@ function load_email(email_id) {
     document.querySelector('#email_body').innerHTML = data.body;
     document.querySelector('#email_timestamp').innerHTML = data.timestamp;    
   });
+
+  //mark email as read
+  fetch(`emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+
+  //Click listener for archive button
+  document.querySelector('#archived_button').addEventListener('click', () => {
+    archive_manager(email_id);
+  })
+}
+
+function archive_manager(email_id) {
+  
+
+  //email is archived, change it and call mailbox('inbox')
+  condition.innerHTML = 'Archived';
+
 }
